8ebb44d73f6212ac97f13864b42e153f67eecbe5
JuanK <jucastro19@poligran.edu.co>
2020-09-29 09:08:37 -0500

    Cambios en front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PokemonListComponent
+    PokemonListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
