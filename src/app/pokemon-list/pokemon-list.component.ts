e121f01c31d5a8e0356ae77efff9e8b2557c174d
JuanK <jucastro19@poligran.edu.co>
2020-09-29 22:18:21 -0500

    correcciones de estilos

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,25 +1,33 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {ApiPokemonService} from '../api-pokemon.service';
+import { ApiPokemonService } from '../api-pokemon.service';
 import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
 export class PokemonListComponent implements OnInit {
-  @Input() url: string = "https://pokeapi.co/api/v2/pokemon?limit=9&offset=0";
-  listPokemon : any[] = [];
+  @Input() url: string = 'https://pokeapi.co/api/v2/pokemon?limit=12&offset=0';
+  listPokemon: any[] = [];
   count;
   nextUrl;
 
-  constructor(private apiPokemonService: ApiPokemonService, private route: ActivatedRoute) {
+  constructor(
+    private apiPokemonService: ApiPokemonService,
+    private route: ActivatedRoute
+  ) {
     this.apiPokemonService.getPokemonService(this.url).subscribe(
       (data) => {
         this.listPokemon = data['results'];
         this.count = data['count'];
         let oldNextUrl = data['next'];
-        this.nextUrl = oldNextUrl.replace(/\:/g,'_v_').replace(/\//g,'_z_').replace(/\?/g,'_x_').replace(/\=/g,'_t_').replace(/\&/g,'_t_');
+        this.nextUrl = oldNextUrl
+          .replace(/\:/g, '_v_')
+          .replace(/\//g, '_z_')
+          .replace(/\?/g, '_x_')
+          .replace(/\=/g, '_t_')
+          .replace(/\&/g, '_t_');
       },
       (error) => {
         console.error(error);
@@ -27,8 +35,5 @@ export class PokemonListComponent implements OnInit {
     );
   }
 
-  ngOnInit() {
-    
-  }
-
+  ngOnInit() {}
 }
