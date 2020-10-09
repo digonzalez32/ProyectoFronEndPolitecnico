c2c81837514f2e101f434525fe809ceaf8611b64
JuanK <jucastro19@poligran.edu.co>
2020-10-09 15:27:30 -0500

    cards

--- src/app/components/poke-detail/poke-detail.component.ts
+++ src/app/components/poke-detail/poke-detail.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-poke-detail',
   templateUrl: './poke-detail.component.html',
-  styleUrls: ['./poke-detail.component.scss']
+  styleUrls: ['./poke-detail.component.scss'],
 })
 export class PokeDetailComponent implements OnInit {
+  pokemon: any = '';
+  pokemonImg = '';
+  pokemonType = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activatedRouter: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {
+    //obtiene parametro de la url
+    this.activatedRouter.params.subscribe((params) => {
+      this.getPokemon(params['id']);
+    });
   }
 
+  ngOnInit(): void {}
+
+  getPokemon(id) {
+    this.pokemonService.getPokemons(id).subscribe(
+      (res) => {
+        console.log(res);
+        this.pokemon = res;
+        this.pokemonImg = this.pokemon.sprites.front_default;
+        this.pokemonType = res.types[0].type.name;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
 }
